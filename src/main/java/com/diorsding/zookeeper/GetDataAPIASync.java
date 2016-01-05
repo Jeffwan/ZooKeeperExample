6d50c5acdad48356bb7b6a7dbe2ddde48b03b76e
Jeffwan <seedjeffwan@gmail.com>
2016-01-04 22:30:06 -0800

    Naive APi get data function

--- src/main/java/com/diorsding/zookeeper/GetDataAPIASync.java
+++ src/main/java/com/diorsding/zookeeper/GetDataAPIASync.java
@@ -1,12 +1,63 @@
 package com.diorsding.zookeeper;
 
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.zookeeper.AsyncCallback;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.data.Stat;
+
+import com.diorsding.zookeeper.constants.Constants;
 
 public class GetDataAPIASync implements Watcher {
 
-	public void process(WatchedEvent event) {
-		
-	}
+	private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+	private static ZooKeeper zookeeper = null;
+	
+	public static void main(String[] args) throws Exception {
+		String path = "/zk-book"; 
 
+		zookeeper = new ZooKeeper(Constants.connectionString, Constants.timeout, new GetDataAPIASync());
+		
+		connectedSemaphore.await();
+		
+		zookeeper.create(path, "123".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+		
+		zookeeper.getData(path, true, new IDataCallback(), null);
+		
+		zookeeper.setData(path, "123".getBytes(), -1);
+		
+		Thread.sleep(Integer.MAX_VALUE);
+	}
+	
+	public void process(WatchedEvent event) {
+		if (KeeperState.SyncConnected == event.getState()) {
+			if (EventType.None == event.getType() && null == event.getPath()) {
+				connectedSemaphore.countDown();
+			} else if (event.getType() == EventType.NodeDataChanged) {
+				try {
+					zookeeper.getData(event.getPath(), true, new IDataCallback(), null);
+					
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
+			}
+		}
+	}
+}
+
+class IDataCallback implements AsyncCallback.DataCallback {
+
+	public void processResult(int rc, String path, Object ctx, byte[] data,
+			Stat stat) {
+		System.out.println(rc + " , " + path + " , " + new String(data));
+		System.out.println(stat.getCzxid() + " , " + stat.getMzxid() + " , " + stat.getVersion());
+	}
+	
 }
