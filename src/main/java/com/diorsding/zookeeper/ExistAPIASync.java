b72d3c6a0afc070d507f9c122a6c64e75eedfd1f
Jeffwan <seedjeffwan@gmail.com>
2016-01-04 22:54:50 -0800

    Naive Exist API Done

---
+++ src/main/java/com/diorsding/zookeeper/ExistAPIASync.java
@@ -0,0 +1,15 @@
+package com.diorsding.zookeeper;
+
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+
+public class ExistAPIASync implements Watcher {
+
+	
+	
+	public void process(WatchedEvent event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/main/java/com/diorsding/zookeeper/ExistAPISync.java
@@ -0,0 +1,64 @@
+package com.diorsding.zookeeper;
+
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
+
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.WatchedEvent;
+import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.Watcher.Event.EventType;
+import org.apache.zookeeper.Watcher.Event.KeeperState;
+import org.apache.zookeeper.ZooDefs.Ids;
+import org.apache.zookeeper.ZooKeeper;
+
+import com.diorsding.zookeeper.constants.Constants;
+
+public class ExistAPISync implements Watcher {
+	
+	private static CountDownLatch connectedSemaphore = new CountDownLatch(1);
+	private static ZooKeeper zookeeper = null;
+	
+	public static void main(String[] args) throws Exception {
+		String path = "/zk-book"; 
+
+		zookeeper = new ZooKeeper(Constants.connectionString, Constants.timeout, new ExistAPISync());
+		connectedSemaphore.await();
+		
+		zookeeper.exists(path, true);
+		zookeeper.create(path, "".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		zookeeper.setData(path, "123".getBytes(), -1);
+		
+		zookeeper.create(path + "/c1", "".getBytes(), Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+		
+		zookeeper.delete(path + "/c1", -1);
+		zookeeper.delete(path, -1);
+		
+		Thread.sleep(Integer.MAX_VALUE);
+	}
+	
+	public void process(WatchedEvent event) {
+		try {
+			if (KeeperState.SyncConnected == event.getState()) {
+				if (EventType.None == event.getType() && null == event.getPath()) {
+					connectedSemaphore.countDown();
+				} else if (EventType.NodeCreated == event.getType()) {
+					System.out.println("Node(" + event.getPath() + ") DataCreated");
+					zookeeper.exists(event.getPath(), true);
+				} else if (EventType.NodeDeleted == event.getType()) {
+					System.out.println("Node(" + event.getPath() + ") DataDeleted");
+					zookeeper.exists(event.getPath(), true);
+				} else if (EventType.NodeDataChanged == event.getType()) {
+					System.out.println("Node(" + event.getPath() + ") DataChanged");
+					zookeeper.exists(event.getPath(), true);
+				}
+			}
+			
+			
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+		
+	}
+
+}
